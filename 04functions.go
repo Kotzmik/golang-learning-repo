e67ed18923ef898c08c64c99fc012eeebef979ea
Kotzmik <47783794+Kotzmik@users.noreply.github.com>
2025-01-31 20:22:11 +0100

    dzień 3

--- 04functions.go
+++ 04functions.go
@@ -2,19 +2,29 @@ package main
 
 import "fmt"
 
-func plus(a int, b int) int {
+func plus(a int, b int) int {return a + b}
+func plusPlus(a, b, c int) int { return a + b + c }
+func vals() (int, int) {return 6,9} // funckja zwracająca więcej ninż 1 wartość
+func sum(nums ...int) {
+    fmt.Print(nums, " ")
+    total:=0
 
-    return a + b
+    for _, num :=range nums { // pierwsza zmienna to index slajsa
+        fmt.Println("num:",num)
+        total += num
+    }
+    // for i:=0; i<len(nums); i++ {
+    //     total+=nums[i]
+    // }
+    fmt.Println(total)
 }
-
-func plusPlus(a, b, c int) int {
-    return a + b + c
+func licznikhehe() func() int { //domknięcie
+    i:=0
+    return func() int {
+        i++
+        return i
+    }
 }
-
-func vals() (int, int) {
-  return 6,9
-}
-
 func main() {
 
     res := plus(1, 2)
@@ -25,5 +35,29 @@ func main() {
 
     a, b :=vals()
     fmt. Println(a, b)
+    sum(1, 2, 3, 5)
+    numerki:=[]int{9,9,7}
+    sum(numerki...)
+    // fmt.Printf("%T", numerki...) sprawdzanie czy to slice, tak nie działa
+    fmt.Println(numerki)
+    
+    nextInt:=licznikhehe()
+    fmt.Println(nextInt())
+    fmt.Println(nextInt())
+    fmt.Println(nextInt())
+    fmt.Println(nextInt())
+    newInt:=licznikhehe()
+    fmt.Println(newInt())
 
+    fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    var fib func(n int) int
+    fib=func(n int) int {
+        if n<2 {
+            return n
+        }
+        return fib(n-1)+fib(n-2)
+    }
+    for i:=2; i<9; i++ {
+    fmt.Println(fib(i))
+    }
 }
